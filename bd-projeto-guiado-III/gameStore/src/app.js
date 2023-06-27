76054849959ed30a7e8ad369779c1a7c48f5c9a6
unknown <luanamellof@hotmail.com>
2023-06-27 19:31:35 -0300

    atv finalizada

--- bd-projeto-guiado-III/gameStore/src/app.js
+++ bd-projeto-guiado-III/gameStore/src/app.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 const mongoose = require("./database/dbConnect");
 const consolesRoutes = require("./routes/consolesRoute");
 const gamesRoutes = require("./routes/gamesRoute");
+const colaboradorasRoutes = require("./routes/colaboradorasRoutes");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/gamestore", colaboradorasRoutes);
 mongoose.connect();
 
 app.use("/gamestore/consoles",consolesRoutes);
