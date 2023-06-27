8b0a2182dd2f826be06ef4228d65ff691565ab02
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-06-27 10:04:20 -0300

    route protection for consoles & games done

--- bd-projeto-guiado-III/gameStore/src/controller/consolesController.js
+++ bd-projeto-guiado-III/gameStore/src/controller/consolesController.js
@@ -4,23 +4,17 @@ const SECRET = process.env.SECRET
 
 const findAllConsoles = async (req, res) => {
   try {
-    
     const authHeader = req.get('authorization')
-    
     if (!authHeader) {
       return res.status(401).send('No authorization info!')
     }
-
     const token = authHeader.split(' ')[1]
-
     jwt.verify(token, SECRET, async function (err) {
       if (err) {
         return res.status(403).send('Access not authorized!')
       }
-
-    const allConsoles = await ConsolesModel.find();
-    res.status(200).json(allConsoles);
-
+      const allConsoles = await ConsolesModel.find();
+      res.status(200).json(allConsoles);
     })
   } catch (err) {
     console.log(err.message);
@@ -30,13 +24,18 @@ const findAllConsoles = async (req, res) => {
 
 const findConsoleById = async (req, res) => {
   try {
-
-
-
-
-
-    const findConsole = await ConsolesModel.findById(req.params.id);
-    res.status(200).json(findConsole);
+    const authHeader = req.get('authorization')
+    if (!authHeader) {
+      return res.status(401).send('No authorization info!')
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, SECRET, async function (err) {
+      if (err) {
+        return res.status(403).send('Access not authorized!')
+      }
+      const findConsole = await ConsolesModel.findById(req.params.id);
+      res.status(200).json(findConsole)
+    })
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error.message });
@@ -45,30 +44,38 @@ const findConsoleById = async (req, res) => {
 
 const addNewConsole = async (req, res) => {
   try {
-    const {
-      name,
-      developer,
-      releaseDate,
-      display,
-      storageCapacities,
-      numberOfPlayers,
-      available,
-      description,
-    } = req.body;
-    const newConsole = new ConsolesModel({
-      name,
-      developer,
-      releaseDate,
-      display,
-      storageCapacities,
-      numberOfPlayers,
-      available,
-      description,
-    });
-
-    const savedConsole = await newConsole.save();
-
-    res.status(201).json({ message: "New console successfully added", savedConsole });
+    const authHeader = req.get('authorization')
+    if (!authHeader) {
+      return res.status(401).send('No authorizarion info')
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, SECRET, async function (err) {
+      if (err) {
+        return res.status(403).send('Access not authorized!');
+      }
+      const {
+        name,
+        developer,
+        releaseDate,
+        display,
+        storageCapacities,
+        numberOfPlayers,
+        available,
+        description,
+      } = req.body;
+      const newConsole = new ConsolesModel({
+        name,
+        developer,
+        releaseDate,
+        display,
+        storageCapacities,
+        numberOfPlayers,
+        available,
+        description,
+      });
+      const savedConsole = await newConsole.save();
+      res.status(201).json({ message: "New console successfully added", savedConsole });
+    })
   } catch (error) {
     console.error(error);
     res.status(500).json(error.message);
@@ -77,28 +84,38 @@ const addNewConsole = async (req, res) => {
 
 const updateConsole = async (req, res) => {
   try {
-    const {
-      name,
-      developer,
-      releaseDate,
-      display,
-      storageCapacities,
-      numberOfPlayers,
-      available,
-      description,
-    } = req.body;
-    const updateConsole = await ConsolesModel.findByIdAndUpdate(req.params.id, {
-      name,
-      developer,
-      releaseDate,
-      display,
-      storageCapacities,
-      numberOfPlayers,
-      available,
-      description,
-    });
-
-    res.status(200).json({ message: "Console successfully updated", updateConsole });
+    authHeader = req.get('authorization')
+    if (!authHeader) {
+      return res.status(401).send('No authorizarion info');
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, SECRET, async function (err) {
+      if (err) {
+        return res.status(403).send('Access not authorized!');
+      }
+      const {
+        name,
+        developer,
+        releaseDate,
+        display,
+        storageCapacities,
+        numberOfPlayers,
+        available,
+        description,
+      } = req.body;
+      const updateConsole = await ConsolesModel.findByIdAndUpdate(req.params.id, {
+        name,
+        developer,
+        releaseDate,
+        display,
+        storageCapacities,
+        numberOfPlayers,
+        available,
+        description,
+      });
+  
+      res.status(200).json({ message: "Console successfully updated", updateConsole });
+    })
   } catch {
     console.error(error);
     res.status(500).json({ message: error.message });
@@ -107,10 +124,20 @@ const updateConsole = async (req, res) => {
 
 const deleteConsole = async (req, res) => {
   try {
-    const { id } = req.params;
-    const deleteConsole = await ConsolesModel.findByIdAndDelete(id);
-    const message = `Console with id ${deleteConsole.name} was successfully deleted`;
-    res.status(200).json({ message });
+    authHeader = req.get('authorization')
+    if (!authHeader) {
+      return res.status(401).send('No authorization info!')
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, SECRET, async function (err) {
+      if (err) {
+        return res.status(403).send('Access not authorized!');
+      }
+      const { id } = req.params;
+      const deleteConsole = await ConsolesModel.findByIdAndDelete(id);
+      const message = `Console with id ${deleteConsole.name} was successfully deleted`;
+      res.status(200).json({ message });
+    })
   } catch (error){
     console.error(error);
     res.status(500).json({ message: error.message });
