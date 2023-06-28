726af6840bc0b6e5004133d12d2d225e68f24f90
Jamile <milli.santana2017@gmail.com>
2023-06-28 14:10:12 -0300

    feat: Adiciona exercicios da semana 14

--- bd-projeto-guiado-III/gameStore/src/app.js
+++ bd-projeto-guiado-III/gameStore/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const mongoose = require("./database/dbConnect");
 const consolesRoutes = require("./routes/consolesRoute");
 const gamesRoutes = require("./routes/gamesRoute");
-
+const colaboradorasRoutes = require("./routes/colaboradorasRoute")
 const app = express();
 
 app.use(express.json());
@@ -13,5 +13,6 @@ mongoose.connect();
 
 app.use("/gamestore/consoles",consolesRoutes);
 app.use("/gamestore/games",gamesRoutes);
+app.use("/gamestore", colaboradorasRoutes);
 
 module.exports = app;
--- bd-projeto-guiado-III/gameStore/src/controller/consolesController.js
+++ bd-projeto-guiado-III/gameStore/src/controller/consolesController.js
@@ -1,9 +1,25 @@
 const ConsolesModel = require("../models/consolesModel");
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
 
 const findAllConsoles = async (req, res) => {
   try {
-    const allConsoles = await ConsolesModel.find();
-    res.status(200).json(allConsoles);
+    const authHeader = req.get("authorization"); // pega o cabeçalho de autorização
+
+    if(!authHeader){
+      return res.status(401).send("Você esqueceu de passar as informações de autorização");
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, 
+      SECRET, 
+      async function(erro){
+      if(erro){
+        return res.status(403).send("Acesso não autorizado.");
+      }
+
+      const allConsoles = await ConsolesModel.find();
+      res.status(200).json(allConsoles);
+    });
   } catch {
     console.log(error);
     res.status(500).json({ message: error.message });
@@ -86,13 +102,14 @@ const deleteConsole = async (req, res) => {
   try {
     const { id } = req.params;
     const deleteConsole = await ConsolesModel.findByIdAndDelete(id);
-    const message = `Console with id ${deleteConsole.name} was successfully deleted`;
+    const message = `Console with id ${deleteConsole.name}, was successfully deleted`;
     res.status(200).json({ message });
   } catch (error){
     console.error(error);
     res.status(500).json({ message: error.message });
   };
 };
+
 
 module.exports = {
   findAllConsoles,
