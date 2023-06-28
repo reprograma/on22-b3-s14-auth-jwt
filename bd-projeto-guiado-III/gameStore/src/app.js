118dbda7b6a3ab9d0c9efbbd01f423856fcbdc87
SarahBernardino799 <sarahberna799@gmail.com>
2023-06-28 15:07:54 -0300

    protegi todas as rotas

--- bd-projeto-guiado-III/gameStore/src/app.js
+++ bd-projeto-guiado-III/gameStore/src/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const mongoose = require("./database/dbConnect");
 const consolesRoutes = require("./routes/consolesRoute");
 const gamesRoutes = require("./routes/gamesRoute");
+const colaboradorasRoute =require("./routes/colaboradorasRoute");
 
 const app = express();
 
@@ -13,5 +14,6 @@ mongoose.connect();
 
 app.use("/gamestore/consoles",consolesRoutes);
 app.use("/gamestore/games",gamesRoutes);
+app.use("/gamestore",colaboradorasRoute);
 
 module.exports = app;
